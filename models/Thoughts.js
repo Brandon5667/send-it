40931a6f5f9efa4f2dd672aef7fea405bcfad888
Brandon Cullifer <brandon.cullifer@gmail.com>
2023-05-29 12:39:45 -0400

    created models

---
+++ models/Thoughts.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+const Reaction = require('./Reaction');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxLength: 280,
+          },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (date) => {
+                if (date) return date.toISOString().split("T") [0];
+              },
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [Reaction]
+    }
+);
+
+thoughtSchema
+    .virtual('reactionCount')
+    .get(function () {
+        return `Number of reactions: ${this.reactions.length}`
+    });
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
